0c3664ccdac73f3476e5dc076a9708e143729d4f
xiyuanzhou <xiyuan.zhou@sjsu.edu>
2022-11-03 10:36:48 -0700

    widget build success and readme updated

--- DjangoClass/my_computer_site/computers/forms.py
+++ DjangoClass/my_computer_site/computers/forms.py
@@ -4,5 +4,4 @@ class ReviewForm(forms.Form):
     first_name = forms.CharField(label='First name', max_length=100)
     last_name = forms.CharField(label='Last name', max_length=100)
     email = forms.EmailField(label='email')
-    review = forms.CharField(label='write your review here')
-    
+    review = forms.CharField(label='write your review here',widget=forms.Textarea(attrs={'class':'myform'}))
