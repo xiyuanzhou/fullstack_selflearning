37b3d9a33199d4325d2651eafa75a0b92f337a68
xiyuanzhou <xiyuan.zhou@sjsu.edu>
2022-11-07 11:02:51 -0800

    updated course note models form

--- DjangoClass/my_computer_site/computers/models.py
+++ DjangoClass/my_computer_site/computers/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Review(models.Model):
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    stars = models.IntegerField(null=True)
